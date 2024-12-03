65c65be1a376c632e5097d3e365069e018abf516
Archie Cayabyab <archiejosephcayabyab@gmail.com>
2024-12-03 23:05:51 +0800

    feat: Added ComplaintTypeDropdown Component and Modified BlotterReport Page and AddIncidenReport Page.

--- frontend/src/pages/AddIncidentReportPage.jsx
+++ frontend/src/pages/AddIncidentReportPage.jsx
@@ -8,10 +8,15 @@ import Sidebar from "../components/Sidebar";
 import Breadcrumbs from "../components/Breadcrumbs";
 import Pagination from '../components/Pagination';
 import Search from '../components/Search';
-
+import ComplaintTypeSelect from "../components/ComplaintTypeDropdown";
 
 const AddIncidentReportPage = () => {
     const [errorMessage, setErrorMessage] = useState(null);
+    const [selectedComlaintType, setSelectedComplaintType] = useState(null);
+
+    const handleComplaintTypeChange = (selectedValue) => {
+        setSelectedComplaintType(selectedValue)
+    };
 
     return (
         <div className="flex flex-col h-screen">
@@ -23,10 +28,32 @@ const AddIncidentReportPage = () => {
                         <Breadcrumbs />
                         <div className="mx-auto bg-white p-10 rounded-lg">
                             <div className="mb-6 leading-3">
-                                <h1 className="text-xl font-semibold text-gray-500">Add New Incident Report</h1>
-                                <p className="text-sm text-gray-400 mt-2">Fill out the form below to add a new incident report to the system.</p>
+                                <h1 className="text-xl font-semibold text-gray-500">Add Complaint</h1>
+                                <p className="text-sm text-gray-400 mt-2">Fill out the form below to add a new complaint to the system.</p>
                             </div>
                             {errorMessage && <p className="text-red-500 text-sm mb-4">{errorMessage}</p>}
+                            <div className="col-span-1 md:col-span-3 mb-4">
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-6  mb-8">
+                                    <div className='flex-1'>
+                                        <label className="block mb-2 text-sm font-medium text-gray-500">Complaint Type<span className="text-red-600">*</span></label>
+                                        <ComplaintTypeSelect onSelect={handleComplaintTypeChange} />
+                                    </div>
+                                </div>
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                                    <div className='flex-1'>
+                                        <label className="block mb-2 text-sm font-medium text-gray-500">Complaint Name<span className="text-red-600">*</span></label>
+                                        <input type="text" name="ComplaintName" placeholder="Type or Search Complainant Name" className="border text-sm border-gray-300 p-2 w-full text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                                    </div>
+                                    <div className='flex-1'>
+                                        <label className="block mb-2 text-sm font-medium text-gray-500">Address<span className="text-red-600">*</span></label>
+                                        <input type="text" name="Address" placeholder="Type Complainant Address" className="border text-sm border-gray-300 p-2 w-full text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                                    </div>
+                                    <div className='flex-1'>
+                                        <label className="block mb-2 text-sm font-medium text-gray-500">Contact Number</label>
+                                        <input type="text" name="ComplaintContactNumber" placeholder="Type Complainant Contact Number" className="border text-sm border-gray-300 p-2 w-full text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </main>
