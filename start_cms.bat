6cadf24cfe56e99f50017281e9a6f9bdf42ba565
Oliver N Valencia <oliver.valencia@Pelatro.com>
2024-12-21 13:26:41 +0800

    1. Allow vite to access via network 2. Add batch file to start server and frontend

---
+++ start_cms.bat
@@ -0,0 +1,13 @@
+@echo off
+set currentDir=%cd%
+echo Starting server.....
+cd %currentDir%/server/
+start start_server.bat
+timeout /t 5 /nobreak
+echo Done
+echo Starting frontend.....
+cd %currentDir%/frontend/
+start start_frontend.bat
+timeout /t 5 /nobreak
+echo Done
+pause
