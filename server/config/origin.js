923ec768b1122b2488591859c85add984b37bc8c
Mark1007 <santosmark4564@gmail.com>
2025-01-15 16:49:45 +0800

    Moved server port config to origin.js

--- server/config/origin.js
+++ server/config/origin.js
@@ -1,7 +1,7 @@
 const cfg = {
   domainname: "colo-cms.gov.ph",
-  ipaddress: "192.168.254.102",
-  serverport: "8082",
+  ipaddress: "192.168.0.125",
+  serverport: "8080",
 };
 
 export default cfg;
