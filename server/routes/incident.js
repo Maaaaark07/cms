9111628524ceeda64d53d5a10346ddc5e42be50d
Archie Cayabyab <archiejosephcayabyab@gmail.com>
2025-01-30 23:40:33 +0800

    feat: Added Update and Get Incident Controller and its Routes.

--- server/routes/incident.js
+++ server/routes/incident.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {
     getAllIncidents,
+    getIncidentById,
     addIncidentReport,
+    updateIncidentReport,
     deleteIncidentReport
 } from "../controllers/incidentController.js";
 import { verifyUser } from "../middleware/authMiddleware.js";
@@ -9,6 +11,8 @@ import { verifyUser } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.get("/get-all-incidents/:id", verifyUser, getAllIncidents);
+router.get("/get-incident/:id", verifyUser, getIncidentById);
 router.post("/add/incident-report", verifyUser, addIncidentReport);
+router.put("/update/incident-report/:id", verifyUser, updateIncidentReport);
 router.delete("/:id", verifyUser, deleteIncidentReport);
 export default router;
