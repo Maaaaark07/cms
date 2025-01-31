8114a6e13241390bd4e2d014cc3a2e4e87b73dcf
Archie Cayabyab <archiejosephcayabyab@gmail.com>
2025-01-31 23:20:24 +0800

    fix: Modified the BlotterList Component.

--- frontend/src/components/BlotterList.jsx
+++ frontend/src/components/BlotterList.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import axios from "axios";
+import cfg from "../../../server/config/domain";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaRegFileLines } from "react-icons/fa6";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { useAuth } from "./AuthContext";
 
 const BlotterList = ({ blotterData }) => {
+
     const navigate = useNavigate();
+    const { barangayId } = useAuth();
+
+    const [loading, setLoading] = useState(false);
+    const [incidentData, setIncidentData] = useState([]);
+
+    useEffect(() => {
+        fetchIncidentDetails();
+    }, [barangayId]);
+
+    const fetchIncidentDetails = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(`http://${cfg.domainname}:${cfg.serverport}/incident/get-all-incidents/${barangayId}`, { withCredentials: true });
+            setIncidentData(response.data);
+        } catch (error) {
+            console.error("Error fetching incident data:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const capitalizeFirstLetter = (string) => {
         if (!string) return "";
@@ -13,6 +38,10 @@ const BlotterList = ({ blotterData }) => {
 
     const handleViewBlotter = (id) => {
         navigate("/Blotter-Report/Blotter-Report-View", { state: { blotter_id: id } });
+    };
+
+    const handleViewIncident = (id) => {
+        navigate("/blotter-report/incident-report-view", { state: { incident_id: id } });
     };
 
     return (
@@ -35,7 +64,7 @@ const BlotterList = ({ blotterData }) => {
                                     {capitalizeFirstLetter(incident.incident_type)}
                                 </span>
                                 <span className="text-sm text-gray-500">
-                                    Incident Report ID:{" "}
+                                    Complaint Report ID:{" "}
                                     <span className="text-blue-500">#{incident.blotter_id}</span>
                                 </span>
                                 <span className="text-sm text-gray-500">
@@ -49,7 +78,39 @@ const BlotterList = ({ blotterData }) => {
                             <FaArrowRightLong className="w-3 h-3 text-blue-600" />
                         </div>
                     </div>
-                ))}
+                ))
+            }
+            {
+                incidentData?.map((incident) => (
+                    <div
+                        key={incident.incident_id}
+                        className="flex items-center bg-blue-50 rounded-md p-4 justify-between mb-4"
+                    >
+                        <div className="flex items-center gap-4">
+                            <div className="bg-blue-200 rounded-full p-4">
+                                <FaRegFileLines className="w-5 h-5 text-blue-600" />
+                            </div>
+                            <div className="leading-3 flex flex-col">
+                                <span className="text-sm font-bold text-gray-500">
+                                    {capitalizeFirstLetter(incident.incident_type)}
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                    Incident Report ID:{" "}
+                                    <span className="text-blue-500">#{incident.incident_id}</span>
+                                </span>
+                                <span className="text-sm text-gray-500">
+                                    Reporter:{" "}
+                                    <span className="text-blue-500">{incident.reporter_name}</span>
+                                </span>
+                            </div>
+                        </div>
+                        <div onClick={() => handleViewIncident(incident.incident_id)} className="flex items-center gap-2 cursor-pointer">
+                            <span className="text-sm text-blue-500">View</span>
+                            <FaArrowRightLong className="w-3 h-3 text-blue-600" />
+                        </div>
+                    </div>
+                ))
+            }
             <Link
                 to="/blotter-report"
                 className="flex items-center w-full justify-center gap-2 mt-4 text-center"
@@ -62,3 +123,11 @@ const BlotterList = ({ blotterData }) => {
 };
 
 export default BlotterList;
+
+function IncidentCard() {
+    return (
+        <div>
+
+        </div>
+    )
+}
